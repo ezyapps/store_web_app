d4c310d234f2a767b5fdf4d259bcc299b61df290
Anisur Rahman <engr.nayan@outlook.com>
2021-12-01 22:40:51 +0600

    log-in process on-progress

--- src/app/common/_services/auth.service.ts
+++ src/app/common/_services/auth.service.ts
@@ -26,9 +26,10 @@ export class AuthService {
     console.log(this.baseUrl + 'login');
     return this.http.post(this.baseUrl + 'login', model).pipe(
       map((response: any) => {
-        const user = response;
-        if (user) {
-          localStorage.setItem('token', user.token);
+        const token = response;
+        console.log(token);
+        if (token) {
+          localStorage.setItem('token', token);
           this.decodeToken();
         }
       })
